3584821e4781b73d9b6acd8bb5a8b74a7d805f3a
manikanthsuri <manikanth.suri@gmail.com>
2023-12-11 08:03:18 +0530

    first commit

---
+++ AllInOne/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AllInOne
+//
+//  Created by Suri Mani kanth on 11/12/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
